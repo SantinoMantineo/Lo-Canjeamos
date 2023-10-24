72e6557cd50676a9f5df36f6f07413e1fd0f9eb4
Santino Mantineo <santinomantineo@gmail.com>
2023-10-24 09:33:48 -0300

    Cambios

--- Client/src/views/home/home.jsx
+++ Client/src/views/home/home.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import style from './Home.module.css'
-import { motion } from 'framer-motion';
-import LoadingAnimation from "./loadingAnimation/loadingAnimation"
 
 const Home = () => {
   return (
     <>
-
-    <div className={style.container}>
-    {/* <motion.div>
-      <LoadingAnimation/>
-    </motion.div> */}
-      Home
-    </div>
     </>
   )
 }
