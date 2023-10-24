5f7c4404dca326dca6e03e2d9806de629dcdbb2c
Santino Mantineo <santinomantineo@gmail.com>
2023-10-24 09:36:54 -0300

    Changes

--- Client/src/views/home/home.jsx
+++ Client/src/views/home/home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import style from './Home.module.css'
 
 const Home = () => {
   return (
     <>
+    <div className={style.container}>
+      Home
+    </div>
     </>
   )
 }
