98596f6ce4da239f146ec594851de535af5fa84a
Santino Mantineo <santinomantineo@gmail.com>
2023-10-25 19:24:37 -0300

    asd

--- Client/src/components/Google/Google.jsx
+++ Client/src/components/Google/Google.jsx
@@ -1,5 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import style from "./Google.module.css"
 
+function Google() {
+  return (
+    <div>Google</div>
+  )
+}
 
-
-
-
+export default Google
