8c115cbdb51d502bff014ae4d5bd8cb9543d653a
maxivalli <maximilianovalli.sc@gmail.com>
2023-10-27 13:01:37 -0300

    viernes

--- Client/src/views/detail/Detail.jsx
+++ Client/src/views/detail/Detail.jsx
@@ -1,28 +1,42 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Header from '../../components/header/Header'
-import Banner3 from '../../assets/banner3.jpg'
-import Banner4 from '../../assets/banner4.jpg'
-import Shoes from "../../assets/shoes.jpeg";
+import Header from "../../components/header/Header";
+import Banner3 from "../../assets/banner3.jpg";
+import Banner4 from "../../assets/banner4.jpg";
+import Shoes from "../../assets/nike.jpeg";
 import style from "./Detail.module.css";
 
-const Detail = ({ onClose }) => {
+const Detail = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          variableWidth: true,
+          className: "slider variable-width",
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
   };
 
   return (
     <>
-      <Header banner1={Banner3} banner2={Banner4}></Header>
+      {/* <Header banner1={Banner3} banner2={Banner4}></Header> */}
       <div className={style.detail}>
-        <h2>Nike Air Jordan</h2>
+        <h3>Nike Air Jordan</h3>
         <div className={style.carousel}>
           <Slider {...settings}>
             <div>
@@ -37,12 +51,15 @@ const Detail = ({ onClose }) => {
           </Slider>
         </div>
         <div className={style.info}>
-        <h3>Ubicación: Elisa, Santa Fe</h3>
-        <h3>Rating usuario: ⭐️⭐️⭐️</h3>
+          <h3>Ubicación: Elisa, Santa Fe</h3>
+          <h3>Rating usuario: ⭐️⭐️⭐️</h3>
         </div>
-        <Link to="/home">
-        <button className={style.button}>Volver</button>
-        </Link>
+        <div className={style.buttons}>
+          <Link to="/home">
+            <button className={style.back}>Volver</button>
+          </Link>
+          <button className={style.match}>Canjear</button>
+        </div>
       </div>
     </>
   );
