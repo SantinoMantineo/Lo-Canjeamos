60279371bc2ba8393e9d129a6b5683f86be29ae3
maxivalli <maximilianovalli.sc@gmail.com>
2023-10-27 13:57:08 -0300

    test

--- Client/src/components/register/Register.jsx
+++ Client/src/components/register/Register.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+
 import React from "react";
 import { useState, useEffect } from "react";
 import style from "./Register.module.css";
--- Client/src/views/login/Login.jsx
+++ Client/src/views/login/Login.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable no-unused-vars */
+
 import { useState } from 'react'
 import React from 'react'
 import style from './Login.module.css'
