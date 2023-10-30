da46a495d80b3a2f52e27eaa3aad3925cfa20d7c
Santino Mantineo <santinomantineo@gmail.com>
2023-10-30 12:21:59 -0300

    Login bug free

--- Client/src/views/welcome/welcome.jsx
+++ Client/src/views/welcome/welcome.jsx
@@ -3,45 +3,45 @@ import styled, { keyframes } from "styled-components";
 
 const sideToSide = keyframes`
   0% {
-    background: black;
-    box-shadow: 0 0 30px black;
-    height: 0px;
-    margin-left: 57.5rem;
+    background: #ffe66d;
+    box-shadow: 0 0 30px #ffe66d;
+    heigth: 0px;
+    opacity: 0%;
   }
   50%{
-    background: black;
-    box-shadow: 0 0 30px black;
-    height: 650px;
-    margin-left: 57.5rem;
+    background: white;
+    box-shadow: 0 0 30px white;
+    height: 67px;
+    opacity: 50%;
   }
   75%{
-    background: black;
-    box-shadow: 0 0 30px black;
-    height: 650px;
-    margin-left: 57.5rem;
+    background: white;
+    box-shadow: 0 0 30px white;
+    height: 67px;
+    opacity: 75%;
   }
   100%{
-    background: black;
-    box-shadow: 0 0 30px black;
-    height: 0px;
-    margin-left: 57.5rem;
+    background: #ffe66d;
+    box-shadow: 0 0 30px #ffe66d;
+    opacity: 100%;
   }
 `;
 
 const WelcomeSliderWrapper = styled.div`
-  width: 20px;
+  width: 250px;
   height: 0px;
-  background: black;
+  background: #ffe66d;
   position: absolute;
-  box-shadow: 0 0 30px black;
-  z-index: 5;
-  animation: ${sideToSide} 5s ease-in-out;
-  margin-left: 57.5rem;
+  box-shadow: 0 0 30px #ffe66d;
+  z-index: 1000;
+  animation: ${sideToSide} 2s ease-in-out;
+  top: 0;
 `;
 
-const Welcome = () => {
+const Welcome = ({userData}) => {
   return (
     <WelcomeSliderWrapper>
+      <h2>{userData && userData.username}</h2>
     </WelcomeSliderWrapper>
   );
 };
