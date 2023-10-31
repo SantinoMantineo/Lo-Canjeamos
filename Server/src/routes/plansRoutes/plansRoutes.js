a1d46d1acd1fad0d00570cdf78605699e734bb2c
Santino Mantineo <santinomantineo@gmail.com>
2023-10-31 16:18:04 -0300

    rutas y controller para Payments

--- Server/src/routes/plansRoutes/plansRoutes.js
+++ Server/src/routes/plansRoutes/plansRoutes.js
@@ -1,13 +1,33 @@
-// const express = require("express");
-// const router = express.Router();
-// const plansRoutes = require("../../controllers/plansControllers");
+const express = require("express");
+const router = express.Router();
+const plansController = require("../../controllers/plansController");
 
-// router.get('/', async(req, res) =>{
-//     try{
-//         const response = await plansRoutes.getPlanById();
-//         return res.status(200).json(response)
-//     } catch(error){
-//         return res.status(400).json(error.message)
-//     }
-// })
-// module.exports = router;
+router.get('/createOrder', async(req, res) =>{
+    try{
+        const response = await plansController.createOrder();
+        return res.status(200).json(response)
+    } catch(error){
+        return res.status(400).json(error.message)
+    }
+})
+
+router.get('/success', async(req, res) =>{
+    try{
+        const response = await plansController.getPlanById();
+        return res.status(200).json(response)
+    } catch(error){
+        return res.status(400).json(error.message)
+    }
+})
+
+
+router.get('/webhook', async(req, res) =>{
+    try{
+        const response = await plansController.getPlanById();
+        return res.status(200).json(response)
+    } catch(error){
+        return res.status(400).json(error.message)
+    }
+})
+
+module.exports = router;
