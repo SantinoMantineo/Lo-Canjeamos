d2a56cc0233a534bd501d5e109b18330389e82e9
Santino Mantineo <santinomantineo@gmail.com>
2023-11-01 14:00:39 -0300

    Bug fixed

--- Server/src/controllers/usersControllers.js
+++ Server/src/controllers/usersControllers.js
@@ -61,7 +61,6 @@ exports.createUser = async (user) => {
         });
 
         const token = jwtGenerator(newUser.id)
-
         return {newUser, token};
       } catch (error) {
         throw new Error("No se pudo crear el usuario");
