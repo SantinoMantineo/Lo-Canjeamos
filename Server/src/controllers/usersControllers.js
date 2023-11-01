80e43f5f50ad298a8248c65a448172b95ba5c54b
Santino Mantineo <santinomantineo@gmail.com>
2023-11-01 18:13:16 -0300

    EMAIL ANDANDO

--- Server/src/controllers/usersControllers.js
+++ Server/src/controllers/usersControllers.js
@@ -60,10 +60,10 @@ exports.createUser = async (user) => {
         });
 
         const token = jwtGenerator(newUser.id)
-        // await transporter.sendMail(registerMail(user))
+        await transporter.sendMail(registerMail(user))
         return {newUser, token};
       } catch (error) {
-        throw new Error("No se pudo crear el usuario");
+        throw new Error(error);
       }
     }
   }
