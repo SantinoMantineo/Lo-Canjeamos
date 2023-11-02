09c6036e30fb87ebb7a77bae59d378f02beaa074
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2023-11-02 16:09:23 +0100

    Template para el mail al registrarse.

--- Server/src/utils/mailObjects.js
+++ Server/src/utils/mailObjects.js
@@ -1,16 +1,12 @@
+const fs = require('fs');
+
+const registerTemplate = fs.readFileSync(__dirname + '/register.html', 'utf8');
+
 const registerMail = (user) => {
   return {
     to: user.email,
     subject: "Registro completado",
-    html: `
-    <b>Felicitaciones ${user.username}! 🎉</b>
-    <br>
-    <p>Completaste exitosamente el formulario de registro 📑</p>
-    <p>Te invitamos a explorar todos los productos que están esperando a ser canjeados por los usuarios de todo el país 🔥</p>
-    <p>Este es un mail automático de verificación así que no lo respondas directamente. Si tenes alguna consulta podes enviarnos un nuevo mail a <i>locanjeamos@gmail.com</i> y responderemos tu consulta a la brevedad 📩.</p>
-    <br>
-    <p>Saludos, LoCanjeamos</p>
-    `,
+    html: registerTemplate,
   };
 };
 
