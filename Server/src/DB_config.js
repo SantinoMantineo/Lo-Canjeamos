deebc1cbd54bab7379562a9d218aa688698240df
maxivalli <maximilianovalli.sc@gmail.com>
2023-11-03 12:03:39 -0300

    local host

--- Server/src/DB_config.js
+++ Server/src/DB_config.js
@@ -6,15 +6,15 @@ const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
 
-/* const sequelize = new Sequelize(`postgres:${DB_USER}:${DB_PASSWORD}@${DB_HOST}/locanjeamos`, {
-  logging: false,
-  native: false,
-}); */
-
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(`postgres:${DB_USER}:${DB_PASSWORD}@${DB_HOST}/locanjeamos`, {
   logging: false,
   native: false,
 });
+
+/* const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false,
+  native: false,
+}); */
 
 const basename = path.basename(__filename);
 
