000826c8aa1380e5a35ea2c185cea95fc937c55d
matecauci15 <matecaucino@gmail.com>
2023-11-05 18:53:44 -0300

    alert login

--- Client/src/views/login/validate.js
+++ Client/src/views/login/validate.js
@@ -1,14 +1,37 @@
-export const validateUser = (username) => {
-    if(!username){
-     return 'Porfavor inserte un usuario valido'
-    }
-    return null
-}
-export const validatePassw = (password) => {
-    if(!password){
-     return 'Complete con una contraseña correcta'
-    }
-    return null
- }
+// export const validateUser = (username) => {
+//     if(!username){
+//      return 'Porfavor inserte un usuario valido'
+//     }
+//     return null
+// }
+// export const validatePassw = (password) => {
+//     if(!password){
+//      return 'Complete con una contraseña correcta'
+//     }
+//     return null
+//  }
+
+//  export const validateLogin = (username, password) => {
+//     if(username && !password){
+//         return 'La contraseña es incorrecta'
+//     } else if (!username){
+//         return 'Porfavor inserte un usuario valido'
+//     }
+//         return null
+
+// } 
  
 
+export const validateLogin = (username, password) => {
+    const errors = {};
+  
+    if (!username) {
+      errors.username = 'Por favor, ingrese un usuario válido';
+    }
+  
+    if (!password) {
+      errors.password = 'La contraseña es incorrecta';
+    }
+  
+    return errors;
+  };
