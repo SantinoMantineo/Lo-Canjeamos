988a1108c52b55a3dcbe7a38f7b646d14184cba9
matecauci15 <matecaucino@gmail.com>
2023-11-05 22:00:22 -0300

    alerts en details

--- Client/src/views/detail/Detail.jsx
+++ Client/src/views/detail/Detail.jsx
@@ -6,7 +6,7 @@ import {motion} from 'framer-motion';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import Swal from "sweetalert2"
 import style from "./Detail.module.css";
 
 const Detail = ({ userData }) => {
@@ -29,19 +29,36 @@ const Detail = ({ userData }) => {
   
   const handleLikeClick = () => {
     if (myPostId) {
-      // Verificar si el usuario ya dio "like" a esta publicación para evitar doble clic
+
       if (!liked) {
-        // Llamar a la acción de Redux para dar "like" a la publicación
+
         dispatch(likePost(myUserId, likedPostId, myPostId, anotherUserId));
-  
-        // Actualizar el estado local para reflejar el "like"
+
         setLiked(true);
+        Swal.fire({
+          title: "Solicitud de canje enviada",
+          text: "Tu solicitud de canje ha sido enviada con éxito.",
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
       }
     } else {
-      alert("Debes seleccionar una de tus publicaciones para intercambiar.");
+      Swal.fire({
+        title: "Aviso",
+        text: "Debes seleccionar una de tus publicaciones para intercambiar.",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonText: "Aceptar",
+        cancelButtonText: 'Cancelar',
+
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "http://localhost:5173/#/login";
+          Swal.close();
+        }
+      });
     }
   };
-  
 
   const settings = {
     dots: true,
