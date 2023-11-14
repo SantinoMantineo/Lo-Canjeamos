dc723ced7294730bee8004b0707adcd8dd509e8d
maxivalli <maximilianovalli.sc@gmail.com>
2023-11-14 14:06:43 -0300

    webhook duplicada fix

--- Server/src/controllers/plansController.js
+++ Server/src/controllers/plansController.js
@@ -62,25 +62,3 @@ exports.webhook = async (data) => {
         };
     }
 }
-
-exports.webhook = async (data) => {
-console.log("AAA", data)
-    try {
-        if (data.type === "payment") {
-            const user = await User.findByPk(currentUserId)
-
-            const premiumNew = await user.update({
-                plan: "premium"
-            });
-            if(premiumNew){
-                return true
-            }
-        } else {
-            throw new Error("Invalid webhook event type");
-        }
-    } catch (error) {
-        return {
-            error: error.message
-        };
-    }
-}
