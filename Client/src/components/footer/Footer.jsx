8cca5eaec613549e344171252fd9c93a5bd9832f
maxivalli <maximilianovalli.sc@gmail.com>
2023-11-17 15:46:31 -0300

    fotter y buttons fixed

--- Client/src/components/footer/Footer.jsx
+++ Client/src/components/footer/Footer.jsx
@@ -1,59 +1,84 @@
-import React from "react";
+import { useState } from "react";
 import style from "./Footer.module.css";
 
 const Footer = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleFooter = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <>
-    <div className={style.footer}>
-      <div className={style.left}>
-        <h3>Acerca de</h3>
-        <p>Terminos y condiciones</p>
-        <p>Nosotros</p>
-      </div>
-      
-      <div className={style.right}>
-        <h3>Contacto</h3>
-        <a href="https://www.instagram.com/locanjeamos/" target="_blank">
-          <img
-            width="24"
-            height="24"
-            src="https://img.icons8.com/color-glass/48/instagram-new--v1.png"
-            alt="instagram-new--v1"
-          />
-          Instagram
-        </a>
-        <a href="mailto:correo@ejemplo.com">
-          <img
-            width="24"
-            height="24"
-            src="https://img.icons8.com/color/48/apple-mail.png"
-            alt="apple-mail"
-          />
-          Centro de ayuda
-        </a>
-      </div>
-      <div className={style.center}>
-        <h3>Desarrollada por</h3>
-        <div className={style.ab}>
-        <div className={style.a}>
-        <a href="https://github.com/maxivalli" target="_blank">Maximiliano Valli</a>
-        <a href="https://github.com/AgusFleitas" target="_blank">Agustin Fleitas</a>
-        <a href="https://github.com/DoutNik" target="_blank">Carlos Emanuel Klema</a>
-        <a href="https://github.com/AlmironJoel" target="_blank">Joel Almiron</a>
+      <button onClick={toggleFooter}>
+        {isExpanded ? (
+          <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/sort-up.png" alt="sort-up"/>
+        ) : (
+          <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/sort-down.png" alt="sort-down"/>
+        )}
+      </button>
+      <div className={`${style.footer} ${isExpanded ? style.expanded : ""}`}>
+        <div className={style.left}>
+          <h3>Acerca de</h3>
+          <p>Terminos y condiciones</p>
+          <p>Nosotros</p>
         </div>
-        <div className={style.b}>
-        <a href="https://github.com/SantinoMantineo" target="_blank">Santino Mantineo</a>
-        <a href="https://github.com/DUGLASPAEZ" target="_blank">Duglas Paez</a>
-        <a href="https://github.com/matecauci15" target="_blank">Mateo Caucino</a>
+
+        <div className={style.right}>
+          <h3>Contacto</h3>
+          <a href="https://www.instagram.com/locanjeamos/" target="_blank">
+            <img
+              width="24"
+              height="24"
+              src="https://img.icons8.com/color-glass/48/instagram-new--v1.png"
+              alt="instagram-new--v1"
+            />
+            Instagram
+          </a>
+          <a href="mailto:correo@ejemplo.com">
+            <img
+              width="24"
+              height="24"
+              src="https://img.icons8.com/color/48/apple-mail.png"
+              alt="apple-mail"
+            />
+            Centro de ayuda
+          </a>
         </div>
+        <div className={style.center}>
+          <h3>Desarrollada por</h3>
+          <div className={style.ab}>
+            <div className={style.a}>
+              <a href="https://github.com/maxivalli" target="_blank">
+                Maximiliano Valli
+              </a>
+              <a href="https://github.com/AgusFleitas" target="_blank">
+                Agustin Fleitas
+              </a>
+              <a href="https://github.com/DoutNik" target="_blank">
+                Carlos Emanuel Klema
+              </a>
+              <a href="https://github.com/AlmironJoel" target="_blank">
+                Joel Almiron
+              </a>
+            </div>
+            <div className={style.b}>
+              <a href="https://github.com/SantinoMantineo" target="_blank">
+                Santino Mantineo
+              </a>
+              <a href="https://github.com/DUGLASPAEZ" target="_blank">
+                Duglas Paez
+              </a>
+              <a href="https://github.com/matecauci15" target="_blank">
+                Mateo Caucino
+              </a>
+            </div>
+          </div>
         </div>
-        
-        
       </div>
-    </div>
-    <div className={style.bottom}>
-      <p>locanjeamos© - Todos los derechos registrados - 2023</p>
-    </div>
+      <div className={style.bottom}>
+        <p>locanjeamos© - Todos los derechos registrados - 2023</p>
+      </div>
     </>
   );
 };
